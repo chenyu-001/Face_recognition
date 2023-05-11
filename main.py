4f720c58e2923fa706ed274a407ac4cdee23b0e5
liy@info2soft.com <liy@info2soft.com>
2023-05-11 09:40:27 +0800

    add requirements.txt

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 import cv2
+import face_recognition
+
+
+def load_data():
+    # Load the known images
+    image_of_person_1 = face_recognition.load_image_file("person_1.jpg")
+    image_of_person_2 = face_recognition.load_image_file("person_2.jpg")
+
+    # Get the face encoding of each person. This can fail if no one is found in the photo.
+    person_1_face_encoding = face_recognition.face_encodings(image_of_person_1)[0]
+    person_2_face_encoding = face_recognition.face_encodings(image_of_person_2)[0]
+
+    # Create a list of known face encodings
+    known_face_encodings = [
+        person_1_face_encoding,
+        person_2_face_encoding
+    ]
+
+    # Load the image we want to check
+    unknown_image = face_recognition.load_image_file("unknown_person.jpg")
+
+    # Get face encodings for any people in the picture
+    face_locations = face_recognition.face_locations(unknown_image)
+    unknown_face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+
+    # Loop through each face found in the unknown image
+    for unknown_face_encoding in unknown_face_encodings:
+        # See if the face is a match for the known face(s)
+        results = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
+
+        # If a match was found in known_face_encodings, just use the first one.
+        if True in results:
+            print("Access granted")
+        else:
+            print("Access denied")
 
 
 def create_and_show_mirror_image(image_path):
