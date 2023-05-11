d56785cb2f464633ff8e8a0280fb3c535490ff33
liy@info2soft.com <liy@info2soft.com>
2023-05-11 10:27:52 +0800

    平滑滤波：读取一张包含噪声的图像，使用不同的平滑滤波器（如均值滤波器、中值滤波器、高斯滤波器等）对其进行滤波，然后比较滤波效果。

--- main.py
+++ main.py
@@ -1,64 +1,40 @@
 import cv2
-import face_recognition
+import matplotlib.pyplot as plt
 
 
-def load_data():
-    # Load the known images
-    image_of_person_1 = face_recognition.load_image_file("person_1.jpg")
-    image_of_person_2 = face_recognition.load_image_file("person_2.jpg")
+def filterEnhance(image_path):
+    # 读取图像
+    img = cv2.imread(image_path, 0)
 
-    # Get the face encoding of each person. This can fail if no one is found in the photo.
-    person_1_face_encoding = face_recognition.face_encodings(image_of_person_1)[0]
-    person_2_face_encoding = face_recognition.face_encodings(image_of_person_2)[0]
+    # 应用均值滤波器
+    mean_filter = cv2.blur(img, (5, 5))
 
-    # Create a list of known face encodings
-    known_face_encodings = [
-        person_1_face_encoding,
-        person_2_face_encoding
-    ]
+    # 应用中值滤波器
+    median_filter = cv2.medianBlur(img, 5)
 
-    # Load the image we want to check
-    unknown_image = face_recognition.load_image_file("unknown_person.jpg")
+    # 应用高斯滤波器
+    gaussian_filter = cv2.GaussianBlur(img, (5, 5), 0)
 
-    # Get face encodings for any people in the picture
-    face_locations = face_recognition.face_locations(unknown_image)
-    unknown_face_encodings = face_recognition.face_encodings(unknown_image, face_locations)
+    # 绘制图像以进行比较
+    plt.figure(figsize=(12, 12))
 
-    # Loop through each face found in the unknown image
-    for unknown_face_encoding in unknown_face_encodings:
-        # See if the face is a match for the known face(s)
-        results = face_recognition.compare_faces(known_face_encodings, unknown_face_encoding)
+    plt.subplot(221), plt.imshow(img, 'gray'), plt.title('Original Noisy Image')
+    plt.xticks([]), plt.yticks([])
 
-        # If a match was found in known_face_encodings, just use the first one.
-        if True in results:
-            print("Access granted")
-        else:
-            print("Access denied")
+    plt.subplot(222), plt.imshow(mean_filter, 'gray'), plt.title('Mean Filter')
+    plt.xticks([]), plt.yticks([])
 
+    plt.subplot(223), plt.imshow(median_filter, 'gray'), plt.title('Median Filter')
+    plt.xticks([]), plt.yticks([])
 
-def create_and_show_mirror_image(image_path):
-    # 读取图片
-    image = cv2.imread(image_path)
+    plt.subplot(224), plt.imshow(gaussian_filter, 'gray'), plt.title('Gaussian Filter')
+    plt.xticks([]), plt.yticks([])
 
-    # 检查图片是否正确读取
-    if image is None:
-        print("Error: Cannot read the image. Please check the file path.")
-        return
+    plt.show()
 
-    # 创建镜像图片
-    mirror_image = cv2.flip(image, 1)
-
-    # 显示原始图片
-    cv2.imshow('Original Image', image)
-
-    # 显示镜像图片
-    cv2.imshow('Mirror Image', mirror_image)
-
-    # 等待按键，然后关闭显示窗口
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    return mean_filter, median_filter, gaussian_filter
 
 
 if __name__ == "__main__":
-    image_path = "img/rich_women.jpg"
-    create_and_show_mirror_image(image_path)
+    image_path = "img/noisy_chunk.jpg"
+    filterEnhance(image_path)
